490c3e4fac9a6aeb68524a0a5f363ac7717d560c
Carolina Alves dos Santos <xcarolina.alves@gmail.com>
2022-06-21 14:27:58 -0300

    fix: get test

--- blog_api/test/blog_api/user/get_test.exs
+++ blog_api/test/blog_api/user/get_test.exs
@@ -1,20 +1,14 @@
 defmodule BloApi.User.GetTest do
   use BlogApi.DataCase
+  import BlogApi.Factory
 
   alias BlogApi.User.Get
-
-  @valid_attrs %{
-    display_name: "Name Displayed",
-    email: "emailtest@gmail.com",
-    password: "123456",
-    image: "image"
-  }
 
   @user_not_found_message {:error, "User not found!"}
 
   describe "call/1" do
     test "when uuid is valid, returns an user" do
-      {:ok, user} = BlogApi.create_user(@valid_attrs)
+      user = insert(:user)
       response = Get.call(user.id)
 
       assert {:ok, user} = response
